c47f8306103f89950a916a331e4bc6bd844790e4
TobiasdeCastilho <tobias20111@live.com>
2022-06-09 00:41:06 -0300

    sim

--- scripts/main.js
+++ scripts/main.js
@@ -4,12 +4,13 @@ class Table {
       this.index
       this.values
       this.properties = {
-         closement: true,
-         comutative: true,
-         neutralElement: true,
-         simetric: true
+         closement: undefined,
+         comutative: undefined,
+         neutralElement: undefined,
+         simetric: undefined
       },
-      this.type = ''
+      this.type = '',
+      this.neutralElementValue = undefined
    }
    cellActivate() {
       const activeCells = document.getElementsByClassName('tableCellActive')
@@ -18,13 +19,17 @@ class Table {
       const activeCell = document.activeElement
       activeCell.classList.add('tableCellActive')
       if (activeCell.id.includes('column')) {
-         const id = String(activeCell.id).replace('inputcolumn', '')
-         document.getElementById('column' + id).classList.add('tableCellActive')
-         document.getElementById('line' + id).classList.add('tableCellActive')
+         const id = String(activeCell.id).replace('column', '')
+         document.getElementById('column'+id).classList.add('tableCellActive')
+         document.getElementById('line'+id).classList.add('tableCellActive')
       } else
-         document.getElementById('cell' + (String(activeCell.id).replace('inputcell', ''))).classList.add('tableCellActive')
+         document.getElementById('cell' + (String(activeCell.id).replace('cell', ''))).classList.add('tableCellActive')
    }
-   createTable() {
+   lineWrite() {
+      const activeCell = document.activeElement
+      document.getElementById('line' + String(activeCell.id).replace('column', '')).value = activeCell.value
+   }
+   tableCreate() {
       const mainDiv = document.createElement('div')
       const tableDefinitions = document.createElement('div')
       tableDefinitions.classList.add('tableDefinitions')
@@ -33,53 +38,60 @@ class Table {
          if (i == 0) {
             tableDefinitionsDiv.id = 'tableReduce'
             tableDefinitionsDiv.innerHTML = `-`
-            tableDefinitionsDiv.onclick = () => this.addTableCoef()
+            tableDefinitionsDiv.onclick = () => {
+               this.size -= 1
+               this.tableCreate()
+            }
          } else if (i == 2) {
             tableDefinitionsDiv.id = 'tableAdd'
             tableDefinitionsDiv.innerHTML = `+`
+            tableDefinitionsDiv.onclick = () => {
+               this.size += 1
+               this.tableCreate()
+            }
          } else {
             tableDefinitionsDiv.id = 'tableSize'
             tableDefinitionsDiv.innerHTML = `${this.size} x ${this.size}`
          }
          tableDefinitions.appendChild(tableDefinitionsDiv)
       }
-      this.size++
+      const length = this.size + 1
       mainDiv.id = 'table'
       mainDiv.classList.add('table')
-      for (let i = 0; i < this.size; i++) {
+      for (let i = 0; i < length; i++) {
          const lineDiv = document.createElement('div')
          lineDiv.classList.add('tableLine')
-         for (let j = 0; j < this.size; j++) {
-            const cellDiv = document.createElement('div')
-            cellDiv.classList.add('tableCell')
+         for (let j = 0; j < length; j++) {
             const cellInput = document.createElement('input')
-            cellInput.className = 'tableCellInput'
+            cellInput.classList.add('tableCell')
             cellInput.type = 'text'
             if (j === 0) {
                cellInput.disabled = true
-               cellDiv.classList.add('tableCellBolder')
+               cellInput.classList.add('tableCellBolder')
                if (i === 0)
-                  cellDiv.id = 'operation'
+                  cellInput.id = 'operation'
                else
-                  cellDiv.id = 'line' + (j + i)
-               cellDiv.classList.add('tableCellBolder')
+                  cellInput.id = 'line' + (j + i)
+               cellInput.classList.add('tableCellBolder')
             } else {
                if (i === 0) {
-                  cellDiv.id = 'column' + j
-                  cellDiv.classList.add('tableCellBolder')
-                  cellInput.classList.add('tableInputColumn')
+                  cellInput.id = 'column' + j
+                  cellInput.classList.add('tableColumnInput')
+                  cellInput.classList.add('tableCellBolder')
                   cellInput.onkeyup = () => this.lineWrite()
                } else {
-                  cellDiv.id = `cell${i}|${j}`
+                  cellInput.id = `cell${i}|${j}`
+                  cellInput.classList.add('tableCellInput')
                   cellInput.classList.add('operatedCells')
                }
                cellInput.onfocus = () => this.cellActivate()
-               if (i == j && j == this.size - 1)
-                  cellInput.onblur = () => this.tableVerify()
+               if (i == j && j == length - 1)
+                  cellInput.onblur = () => {
+                     this.tableLoad()
+                     this.tableVerify()
+                  }
             }
-            cellInput.id = 'input' + cellDiv.id
-            cellDiv.appendChild(cellInput)
-            lineDiv.appendChild(cellDiv)
+            lineDiv.appendChild(cellInput)
          }
          mainDiv.appendChild(lineDiv)
       }
@@ -89,35 +101,105 @@ class Table {
       tableMain.appendChild(mainDiv)
       document.body.appendChild(tableMain)
       document.body.appendChild(tableDefinitions)
-      document.getElementById('inputcolumn1').focus()
+      document.getElementById('column1').focus()
    }
-   lineWrite() {
-      const activeCell = document.activeElement
-      document.getElementById('inputline' + String(activeCell.id).replace('inputcolumn', '')).value = activeCell.value
-   }
-   tableVerify() {
-      const columns = document.getElementsByClassName('tableInputColumn')
-      const length = columns.length
-      this.index = []
+   tableLoad() {
+      this.tableLoadIndex()
+      const cells = document.getElementsByClassName('tableCellInput')
       this.values = []
-      for (let i = 0; i < length; i++) {
-         this.index.push([columns[i].value, true])
+      for (let i = 0; i < this.size; i++) {
          this.values.push(i)
          this.values[i] = []
-         for (let j = 0; j < length; j++){
+         const line = i*this.size
+         for (let j = 0; j < this.size; j++){
             this.values[i].push(j)
-            this.values[i][j] = document.getElementById(`cell${i+1}|${j+1}`).childNodes[0].value
+            this.values[i][j] = cells[line+j].value
          }
       }
-      for(let i = 0;i < length;i++){
-         for(let j=0;j < length;j++){
-            if (this.type.closement) {
-               if (!(this.type.closement = this.index.includes(this.values[i][j])))
-                  this.type.comutative = false
-               else if (this.type.comutative && j < length-1)
-                  this.type.comutative = (this.values[this.index.indexOf(this.values[i][j])][j+1] === this.values[this.index.indexOf(this.values[i][j+1])][j])
+   }
+   tableLoadIndex() {
+      const columns = document.getElementsByClassName('tableColumnInput')
+      this.index = []
+      for (let i = 0; i < this.size; i++)
+         this.index.push(columns[i].value)
+   }
+   tableVerify() {
+      let neutralLine = []
+      let neutralColumn = []
+      this.properties.closement = this.properties.comutative = true
+      this.properties.neutralElement = false
+      for(let i = 0;i < this.size;i++){
+         for(let j=0;j < this.size;j++){
+            if (this.properties.closement) {
+               if (!this.index.includes(this.values[i][j]))
+                  this.properties.closement = this.properties.comutative = false
+               else if (this.properties.comutative && j < this.size-1){
+                  const index = this.index.indexOf(this.values[i][j+1])
+                  if(index === -1)
+                     this.properties.comutative = false
+                  else
+                     this.properties.comutative = (this.values[this.index.indexOf(this.values[i][j])][j+1] === this.values[index][j])
+               }
             }
+            if(this.values[i][j] === this.index[i])
+            if(!neutralColumn[j])
+                  neutralColumn[j] = 1
+                  else
+                  neutralColumn[j]++
+            if(this.values[i][j] === this.index[j])
+               if(!neutralLine[i])
+                  neutralLine[i] = 1
+               else
+                  neutralLine[i]++
          }
       }
+      for(let i = 0;i < this.size;i++)
+      if(neutralColumn[i] === this.size && neutralLine[i] === this.size){
+            this.properties.neutralElement = true
+            break
+         }
+      this.type = 'none'
+      if(this.properties.closement){
+         this.type = 'groupoid'
+         if(this.properties.comutative){
+            this.type = 'semi-group'
+            if(this.properties.neutralElement){
+               this.type = 'monoid'
+               if(this.properties.idempotent)
+                  this.type = 'group'
+            }
+         }        
+      }
+      return this.type
+   }
+   tableOperate(operation) {
+      this.tableLoadIndex()
+      operation = operation.split('')
+      const aPosition = []
+      const bPosition = []
+      operation.map((value, index) => {
+         if (value == 'a')
+            aPosition.push(index)
+         else if (value == 'b')
+            bPosition.push(index)
+      })
+      const table = this
+      this.values = []
+      this.values.fill([])
+      for(let i=0;i<this.size;i++){
+         this.values.push(i)
+         this.values[i] = []
+         for(let j=0;j<this.size;j++){
+            aPosition.map((value,) => operation[value] = table.index[i])
+            bPosition.map((value,) => operation[value] = table.index[j])
+            this.values[i][j] = String(eval(operation.join('')))
+         }
+      }
+      this.tableWrite()
+   }
+   tableWrite(){
+      for(let i=0;i<this.size;i++)
+         for(let j=0;j<this.size;j++)
+            document.getElementById(`cell${i+1}|${j+1}`).value = this.values[i][j]
    }
 }
